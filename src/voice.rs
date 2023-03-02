a89b559bfcb474d59b1d07fe4fe662d21e3a1d51
sagan <camjanssen120@gmail.com>
2023-03-02 15:52:19 -0700

    join on play

--- src/voice.rs
+++ src/voice.rs
@@ -50,48 +50,56 @@ async fn play(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
     let guild = msg.guild(&ctx.cache).unwrap();
     let guild_id = guild.id;
 
+    let channel_id = guild
+        .voice_states
+        .get(&msg.author.id)
+        .and_then(|voice_state| voice_state.channel_id);
+
+    let connect_to = match channel_id {
+        Some(channel) => channel,
+        None => {
+            check_msg(msg.reply(ctx, "Not in a voice channel").await);
+
+            return Ok(());
+        },
+    };
+
     let manager = songbird::get(ctx)
         .await
         .expect("Songbird Voice client placed in at initialisation.")
         .clone();
 
+    let (handle_lock, _) = manager.join(guild_id, connect_to).await;
+
     if !query.starts_with("http") {
         query = format!("ytsearch:{}", query);
     }
 
-    if let Some(handler_lock) = manager.get(guild_id) {
-        let mut handler = handler_lock.lock().await;
+    let mut handler = handle_lock.lock().await;
 
-        // Here, we use lazy restartable sources to make sure that we don't pay
-        // for decoding, playback on tracks which aren't actually live yet.
-        let source = match Restartable::ytdl(query, true).await {
-            Ok(source) => source,
-            Err(why) => {
-                println!("Err starting source: {:?}", why);
+    // Here, we use lazy restartable sources to make sure that we don't pay
+    // for decoding, playback on tracks which aren't actually live yet.
+    let source = match Restartable::ytdl(query, true).await {
+        Ok(source) => source,
+        Err(why) => {
+            println!("Err starting source: {:?}", why);
 
-                check_msg(msg.channel_id.say(&ctx.http, "Error sourcing ffmpeg").await);
+            check_msg(msg.channel_id.say(&ctx.http, "Error sourcing ffmpeg").await);
 
-                return Ok(());
-            },
-        };
+            return Ok(());
+        },
+    };
 
-        handler.enqueue_source(source.into());
+    handler.enqueue_source(source.into());
 
-        check_msg(
-            msg.channel_id
-                .say(
-                    &ctx.http,
-                    format!("Added song to queue: position {}", handler.queue().len()),
-                )
-                .await,
-        );
-    } else {
-        check_msg(
-            msg.channel_id
-                .say(&ctx.http, "Not in a voice channel to play in")
-                .await,
-        );
-    }
+    check_msg(
+        msg.channel_id
+            .say(
+                &ctx.http,
+                format!("Added song to queue: position {}", handler.queue().len()),
+            )
+            .await,
+    );
 
     Ok(())
 }
