1d3c1d6dbd379dfc6b61c15f61baa92caa31af91
sagan <camjanssen120@gmail.com>
2023-03-04 22:09:47 -0700

    test

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::env;
 use std::sync::Arc;
 use std::sync::atomic::AtomicBool;
 
-use ggstdl::Character;
+//use ggstdl::Character;
 use serenity::async_trait;
 use serenity::model::prelude::Message;
 use serenity::prelude::*;
@@ -16,7 +16,7 @@ mod voice;
 use crate::voice::*;
 
 #[group]
-#[commands(leave, play, skip, repeat, frames)]
+#[commands(leave, play, skip, repeat, /*frames*/)]
 struct General;
 struct Handler;
 
@@ -28,10 +28,10 @@ impl TypeMapKey for RepeatTrack {
     type Value = Arc<AtomicBool>;
 }
 
-struct GGSTDLCharacterData;
-impl TypeMapKey for GGSTDLCharacterData {
-    type Value = Arc<RwLock<Vec<Character>>>;
-}
+// struct GGSTDLCharacterData;
+// impl TypeMapKey for GGSTDLCharacterData {
+//     type Value = Arc<RwLock<Vec<Character>>>;
+// }
 
 #[tokio::main]
 async fn main() {
@@ -52,8 +52,8 @@ async fn main() {
         let mut data = client.data.write().await;
         data.insert::<RepeatTrack>(Arc::new(AtomicBool::new(false)));
 
-        let chars = ggstdl::load().await.expect("Could not load ggstdl character data");
-        data.insert::<GGSTDLCharacterData>(Arc::new(RwLock::new(chars)));
+        // let chars = ggstdl::load().await.expect("Could not load ggstdl character data");
+        // data.insert::<GGSTDLCharacterData>(Arc::new(RwLock::new(chars)));
     }
 
     if let Err(why) = client.start().await {
@@ -61,62 +61,62 @@ async fn main() {
     }
 }
 
-#[command]
-#[only_in(guilds)]
-async fn frames(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
+// #[command]
+// #[only_in(guilds)]
+// async fn frames(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
 
-    if args.len() < 2 {
-        check_msg(msg.channel_id.say(&ctx.http, ".frames <character> <move query>").await);
-        return Ok(());
-    }
+//     if args.len() < 2 {
+//         check_msg(msg.channel_id.say(&ctx.http, ".frames <character> <move query>").await);
+//         return Ok(());
+//     }
 
-    let Ok(char_query) = args.single::<String>() else {
-        check_msg(msg.channel_id.say(&ctx.http, ".frames <character> <move query>").await);
-        return Ok(());
-    };
-    let Some(move_query) = args.remains() else {
-        check_msg(msg.channel_id.say(&ctx.http, ".frames <character> <move query>").await);
-        return Ok(());
-    };
+//     let Ok(char_query) = args.single::<String>() else {
+//         check_msg(msg.channel_id.say(&ctx.http, ".frames <character> <move query>").await);
+//         return Ok(());
+//     };
+//     let Some(move_query) = args.remains() else {
+//         check_msg(msg.channel_id.say(&ctx.http, ".frames <character> <move query>").await);
+//         return Ok(());
+//     };
 
-    // want to drop the locks and refs asap so other threads can use it
-    let move_found = {
-        let data_read = ctx.data.read().await;
-        let char_data_lock = data_read.get::<GGSTDLCharacterData>().expect("No ggstdl character data in TypeMap").clone();
-        let char_data = char_data_lock.read().await;
+//     // want to drop the locks and refs asap so other threads can use it
+//     let move_found = {
+//         let data_read = ctx.data.read().await;
+//         let char_data_lock = data_read.get::<GGSTDLCharacterData>().expect("No ggstdl character data in TypeMap").clone();
+//         let char_data = char_data_lock.read().await;
 
-        let character = char_data.iter().find(|c| c.regex.is_match(char_query.as_str()));
+//         let character = char_data.iter().find(|c| c.regex.is_match(char_query.as_str()));
 
-        let Some(character) = character else {
-            check_msg(msg.channel_id.say(&ctx.http, "could not find character").await);
-            return Ok(());
-        };
+//         let Some(character) = character else {
+//             check_msg(msg.channel_id.say(&ctx.http, "could not find character").await);
+//             return Ok(());
+//         };
 
-        let move_found = character.moves.iter().find(|m| m.regex.is_match(move_query));
-        let Some(move_found) = move_found else {
-            check_msg(msg.channel_id.say(&ctx.http, "could not find move").await);
-            return Ok(());
-        };
+//         let move_found = character.moves.iter().find(|m| m.regex.is_match(move_query));
+//         let Some(move_found) = move_found else {
+//             check_msg(msg.channel_id.say(&ctx.http, "could not find move").await);
+//             return Ok(());
+//         };
 
-        move_found.clone()
-    };
+//         move_found.clone()
+//     };
 
-    let v = msg.channel_id.send_message(&ctx.http, |m| {
-        m.embed(|e| {
-            e.title(move_found.name)
-                .field("Damage", move_found.damage, true)
-                .field("Guard", move_found.guard, true)
-                .field("Startup", move_found.startup, true)
-                .field("Active", move_found.active, true)
-                .field("Recovery", move_found.recovery, true)
-                .field("On Block", move_found.onblock, true)
-                .field("Invuln", move_found.invuln, true)
-        })
-    }).await;
-    check_msg(v);
+//     let v = msg.channel_id.send_message(&ctx.http, |m| {
+//         m.embed(|e| {
+//             e.title(move_found.name)
+//                 .field("Damage", move_found.damage, true)
+//                 .field("Guard", move_found.guard, true)
+//                 .field("Startup", move_found.startup, true)
+//                 .field("Active", move_found.active, true)
+//                 .field("Recovery", move_found.recovery, true)
+//                 .field("On Block", move_found.onblock, true)
+//                 .field("Invuln", move_found.invuln, true)
+//         })
+//     }).await;
+//     check_msg(v);
 
-    Ok(())
-}
+//     Ok(())
+// }
 
 // Checks that a message successfully sent; if not, then logs why to stdout.
 fn check_msg(result: SerenityResult<Message>) {
